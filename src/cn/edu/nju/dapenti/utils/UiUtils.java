2fb8d89000723d6c9a1e97109f0478f26e7b9a99
Jawinton <tangwenjie0@gmail.com>
2013-10-23 20:37:04 +0800

    2.0
    net.fred.feedex to cn.edu.nju.dapenti

---
+++ src/cn/edu/nju/dapenti/utils/UiUtils.java
@@ -0,0 +1,18 @@
+package cn.edu.nju.dapenti.utils;
+
+import cn.edu.nju.dapenti.MainApplication;
+import android.app.Activity;
+import android.util.TypedValue;
+
+
+public class UiUtils {
+    static public void setPreferenceTheme(Activity a) {
+        if (!PrefUtils.getBoolean(PrefUtils.LIGHT_THEME, true)) {
+            a.setTheme(android.R.style.Theme_Holo);
+        }
+    }
+
+    static public int dpToPixel(int dp) {
+        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, MainApplication.getContext().getResources().getDisplayMetrics());
+    }
+}
