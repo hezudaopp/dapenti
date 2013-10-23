640d15d6ec2cd1f11adef9576bf16a7008730a91
Jawinton <tangwenjie0@gmail.com>
2013-10-23 15:22:00 +0800

    2.0
    based on FeedEx

--- gen/cn/edu/nju/dapenti/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package cn.edu.nju.dapenti;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- src/cn/edu/nju/dapenti/menu/handler/MenuHandlerInterface.java
+++
@@ -1,7 +0,0 @@
-package cn.edu.nju.dapenti.menu.handler;
-
-import android.content.Context;
-
-public interface MenuHandlerInterface {
-	public void handle(Context context);
-}
